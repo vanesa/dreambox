54401dfdc05548bc5376da13b48a3db3c84a5448
Vanesa <vanesa@gmx.net>
2015-09-13 23:43:53 -0700

    Added working previous and next links.

--- dreambox.js
+++ dreambox.js
@@ -5,9 +5,30 @@ function callbackFunction(json) {
 	window.current_photo = 0;
     var photo = photos[current_photo];
     var photo_url = photo.images.standard_resolution.url
-    console.log(photo_url);
     var lightbox = document.getElementById('lightbox');
-    console.log(lightbox);
     lightbox.innerHTML = '<img src="' + photo_url + '" />';
+};
 
+document.getElementById('previous-photo').onclick = function(){
+    if (window.current_photo == 0) {
+    	return false;
+    }
+    window.current_photo -= 1;
+    var photo = photos[current_photo];
+    var photo_url = photo.images.standard_resolution.url
+    var lightbox = document.getElementById('lightbox');
+	lightbox.innerHTML = '<img src="' + photo_url + '" />';
+    return false;
+};
+
+document.getElementById('next-photo').onclick = function(){
+	if (window.current_photo >= window.photos.length - 1){
+		return false;
+	}
+    window.current_photo += 1;
+    var photo = photos[current_photo];
+    var photo_url = photo.images.standard_resolution.url
+    var lightbox = document.getElementById('lightbox');
+	lightbox.innerHTML = '<img src="' + photo_url + '" />';
+    return false;
 };
