5f90c14db39a7bd9db8354d5eae04d32750ee8cf
Vanesa <vanesa@gmx.net>
2015-09-15 17:20:22 -0700

    Added renderPhoto function, changed id names.

--- dreambox.js
+++ dreambox.js
@@ -2,33 +2,33 @@
 function callbackFunction(json) {
 	console.log(json);
 	window.photos = json.data;
+	window.photos = [photos[0], photos[1], photos[2]];
 	window.current_photo = 0;
-    var photo = photos[current_photo];
-    var photo_url = photo.images.standard_resolution.url
-    var lightbox = document.getElementById('lightbox');
-    lightbox.innerHTML = '<img src="' + photo_url + '" />';
+	renderPhoto();
 };
 
-document.getElementById('previous-photo').onclick = function(){
+function renderPhoto() {
+	var photo = photos[current_photo];
+    var photo_url = photo.images.standard_resolution.url;
+    var lightbox = document.getElementById('lightbox');
+    lightbox.innerHTML = '<img src="' + photo_url + '" />';
+}
+
+document.getElementById('previous-button').onclick = function(){
     if (window.current_photo == 0) {
     	return false;
     }
     window.current_photo -= 1;
-    var photo = photos[current_photo];
-    var photo_url = photo.images.standard_resolution.url
-    var lightbox = document.getElementById('lightbox');
-	lightbox.innerHTML = '<img src="' + photo_url + '" />';
+    renderPhoto();
     return false;
 };
 
-document.getElementById('next-photo').onclick = function(){
+document.getElementById('next-button').onclick = function(){
 	if (window.current_photo >= window.photos.length - 1){
 		return false;
 	}
     window.current_photo += 1;
-    var photo = photos[current_photo];
-    var photo_url = photo.images.standard_resolution.url
-    var lightbox = document.getElementById('lightbox');
-	lightbox.innerHTML = '<img src="' + photo_url + '" />';
+    renderPhoto();
     return false;
 };
+
