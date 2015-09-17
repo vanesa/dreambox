668f36f32b1a3fc2fb134bf2028717e84e26b2f4
Vanesa <vanesa@gmx.net>
2015-09-17 00:11:55 -0700

    Disabled nav buttons when no more photos are available.

--- dreambox.js
+++ dreambox.js
@@ -11,19 +11,40 @@ Element.prototype.remove = function() {
     this.parentElement.removeChild(this);
 }
 
+function updateNav() {
+	var previous_button = document.getElementById('previous-button');
+	var next_button = document.getElementById('next-button');
+	if (current_photo == 0) {
+		previous_button.removeAttribute('href'); 
+		return false;
+	}
+	else {
+		previous_button.setAttribute('href', '#');
+	}
+
+	if (current_photo >= photos.length -1) {
+		next_button.removeAttribute('href'); 
+		return false;
+	}
+	else {
+		next_button.setAttribute('href', '#');
+	}
+};
+
 function renderPhoto() {
+	updateNav();
 	var photo = photos[current_photo];
     var photo_url = photo.images.standard_resolution.url;
     var lightbox = document.getElementById('lightbox');
     var lightbox_img = document.getElementById('lightbox-img');
     if (lightbox_img && lightbox_img.classList.contains("fadein")){
     	document.getElementById("lightbox-img").remove();
-    	console.log('Removed class');
     }
     lightbox.innerHTML = '<img id="lightbox-img" src="' + photo_url + '" />';
     var lightbox_img = document.getElementById('lightbox-img');
     lightbox_img.setAttribute('class', 'fadein');
 }
+
 
 document.getElementById('previous-button').onclick = function(){
     if (window.current_photo == 0) {
