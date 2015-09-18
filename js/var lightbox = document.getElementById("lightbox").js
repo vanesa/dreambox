e1619bb638de2eb39ece020374a6b234fd50cc7a
Vanesa <vanesa@gmx.net>
2015-09-17 20:49:48 -0700

    Added regorganized files to the commit, styled the lightbox, added instagram link to photo.

---
+++ js/var lightbox = document.getElementById("lightbox").js
@@ -0,0 +1,69 @@
+
+function callbackFunction(json) {
+	console.log(json);
+	window.photos = json.data;
+	window.photos = [photos[0], photos[1], photos[2]];
+	window.current_photo = 0;
+	renderPhoto();
+};
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+
+function updateNav() {
+	var previous_button = document.getElementById('previous-button');
+	var next_button = document.getElementById('next-button');
+	if (current_photo == 0) {
+		previous_button.removeAttribute('href'); 
+		return false;
+	}
+	else {
+		previous_button.setAttribute('href', '#');
+	}
+
+	if (current_photo >= photos.length -1) {
+		next_button.removeAttribute('href'); 
+		return false;
+	}
+	else {
+		next_button.setAttribute('href', '#');
+	}
+};
+
+function renderPhoto() {
+	updateNav();
+	var photo = photos[current_photo];
+    var photo_url = photo.images.standard_resolution.url;
+    var photo_title = photo.caption.text;
+    var lightbox = document.getElementById('lightbox');
+    var title = document.getElementById('title');
+    var lightbox_img = document.getElementById('lightbox-img');
+    if (lightbox_img && lightbox_img.classList.contains("fadein")){
+    	document.getElementById("lightbox-img").remove();
+    }
+    lightbox.innerHTML = '<img id="lightbox-img" src="' + photo_url + '" />';
+    title.innerHTML = photo_title;
+    var lightbox_img = document.getElementById('lightbox-img');
+    lightbox_img.setAttribute('class', 'fadein');
+}
+
+
+document.getElementById('previous-button').onclick = function(){
+    if (window.current_photo == 0) {
+    	return false;
+    }
+    window.current_photo -= 1;
+    renderPhoto();
+    return false;
+};
+
+document.getElementById('next-button').onclick = function(){
+	if (window.current_photo >= window.photos.length - 1){
+		return false;
+	}
+    window.current_photo += 1;
+    renderPhoto();
+    return false;
+};
+
